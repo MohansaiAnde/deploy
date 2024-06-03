561e2990a83608745843644bcd68686f83dbcc27
MohansaiAnde <mohan.ande01@gmail.com>
2024-06-03 11:12:22 +0530

    asp.net files added

--- AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
- 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyTitle("Your Project Title")]
-[assembly: AssemblyDescription("Your Project Description")]
-[assembly: AssemblyCopyright("Your Project Copyright")]
